2df58be4124f7a89cc62dcea4b2bb6c303c812e6
KingJ <KingJ@SMG50139.niaccit.niacc.edu>
2024-04-03 11:55:22 -0500

    Added JavaDocs for ConfigReader

--- MacroBuilder/src/database/ConfigReader.java
+++ MacroBuilder/src/database/ConfigReader.java
@@ -9,26 +9,40 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- *
+ * Allows config file to be read
  * @author KingJ
  */
 public class ConfigReader {
 
     private Properties prop;
-
+    
+    /**
+     * Takes in file path and loads file
+     * @param path
+     * @throws IOException 
+     */
     public ConfigReader(String path) throws IOException {
         prop = new Properties();
         prop.load(new FileInputStream(path));
     }
-
+    /**
+     * returns url credential for database
+     * @return 
+     */
     public String getUrl() {
         return prop.getProperty("db.url");
     }
-
+    /**
+     * returns username credential for database
+     * @return 
+     */
     public String getUsername() {
         return prop.getProperty("db.username");
     }
-
+    /**
+     * returns password credential for database
+     * @return 
+     */
     public String getPassword() {
         return prop.getProperty("db.password");
     }
