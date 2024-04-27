b5c9afc5db3fc02de40c5001d0bb0dcc2162ecb7
sweet <sweet@DESKTOP-AGA9A7S>
2024-04-27 00:03:48 -0500

    fixed visibility

--- MacroBuilder/src/controllers/MenuController.java
+++ MacroBuilder/src/controllers/MenuController.java
@@ -859,7 +859,7 @@ public class MenuController implements Initializable {
                 Float.parseFloat(quickAddTextProtein.getText()), user.getCalendar().getDate());
 
         quickAddVBox.setVisible(!quickAddVBox.isVisible());
-
+        quickAddRectangle.setVisible(!quickAddRectangle.isVisible());
         quickAddTextCalories.clear();
         quickAddTextFat.clear();
         quickAddTextCarbs.clear();
